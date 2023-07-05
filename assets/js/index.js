48bae43beb40c363e5b274426d2227c56dd21be5
Pablo Espinoza <astragant97@yahoo.com>
2023-07-04 21:18:19 -0400

    Update index.js
    solucion a error: no se reseteaba el estado de reproduccion

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,38 @@
-// * ------------------------------------------------------------------
 // 02: Declaration of constants and variables
 let song = document.getElementById("song");
 const background = document.querySelector('#fondo'); // background derived from album cover below
 let thumbnail = document.querySelector("#thumbnail");
 
-
-const songArtist = document.querySelector(".player__artist"); 
+const songArtist = document.querySelector(".player__artist");
 const songTitle = document.querySelector(".player__song");
 const progressBar = document.querySelector(".player__level");
 
 let songIndex = 0;
-let songs = ["./assets/media/track1.mp3", "./assets/media/track2.mp3"]; 
-let thumbnails = ["./assets/img/cover1.png", "./assets/img/cover2.jpg"]; 
-let songArtists = ["Devil Electric", "Royal Blood"]; 
+let songs = ["./assets/media/track1.mp3", "./assets/media/track2.mp3"];
+let thumbnails = ["./assets/img/cover1.png", "./assets/img/cover2.jpg"];
+let songArtists = ["Devil Electric", "Royal Blood"];
 let songTitles = ["The Dove and the serpent", "Out of the Black"];
 let playing = true;
 
-
 function playPause() {
-  
   if (playing) {
     thumbnail = document.querySelector('#thumbnail');
     song = document.querySelector("#song");
-   
+
     const playPause = document.getElementById("play");
-    
+
     playPause.querySelector(".pause-btn").classList.toggle("hide");
     playPause.querySelector(".play-btn").classList.toggle("hide");
     thumbnail.style.transform = "scale(1.15)";
-   
+
     song.play();
     playing = false;
-
   } else {
-
     const playPause = document.getElementById("play");
 
     playPause.querySelector(".play-btn").classList.toggle("hide");
     playPause.querySelector(".pause-btn").classList.toggle("hide");
-    thumbnail.style.transform = "scale(1)"
+    thumbnail.style.transform = "scale(1)";
 
     song.pause();
     playing = true;
@@ -50,11 +44,9 @@ song.addEventListener("ended", function () {
 });
 
 function nextSong() {
-  
   songIndex++;
   if (songIndex > 1) {
     songIndex = 0;
-    
   }
 
   song.src = songs[songIndex];
@@ -66,37 +58,8 @@ function nextSong() {
   playing = true;
 
   playPause();
-
+  resetPlayButton();
 }
-
-function cambiar(){
-  if (playing) {
-    thumbnail = document.querySelector('#thumbnail');
-    song = document.querySelector("#song");
-   
-    const playPause = document.getElementById("play");
-    
-    playPause.querySelector(".pause-btn").classList.toggle("hide");
-    playPause.querySelector(".play-btn").classList.toggle("hide");
-    thumbnail.style.transform = "scale(1.15)";
-   
-    song.play();
-    playing = false;
-
-  } else {
-
-    const playPause = document.getElementById("play");
-
-    playPause.querySelector(".play-btn").classList.toggle("hide");
-    playPause.querySelector(".pause-btn").classList.toggle("hide");
-    thumbnail.style.transform = "scale(1)"
-
-    song.pause();
-    playing = true;
-  }
-}
-
-
 
 function previousSong() {
   songIndex--;
@@ -104,7 +67,6 @@ function previousSong() {
     songIndex = 1;
   }
 
-
   song.src = songs[songIndex];
   thumbnail.src = thumbnails[songIndex];
 
@@ -113,24 +75,28 @@ function previousSong() {
 
   playing = true;
   playPause();
+  resetPlayButton();
+}
+
+function resetPlayButton() {
+  const playPause = document.getElementById("play");
+  playPause.querySelector(".play-btn").classList.remove("hide");
+  playPause.querySelector(".pause-btn").classList.add("hide");
+  thumbnail.style.transform = "scale(1)";
 }
 
 function updateProgressValue() {
   progressBar.max = song.duration;
   progressBar.value = song.currentTime;
-  document.querySelector(".start").innerHTML = formatTime(
-    Math.floor(song.currentTime)
-  );
+  document.querySelector(".start").innerHTML = formatTime(Math.floor(song.currentTime));
   if (document.querySelector(".end").innerHTML === "NaN:NaN") {
     document.querySelector(".end").innerHTML = "0:00";
   } else {
-    document.querySelector(".end").innerHTML = formatTime(
-      Math.floor(song.duration)
-    );
+    document.querySelector(".end").innerHTML = formatTime(Math.floor(song.duration));
   }
 }
 
-// convertiir song.currentTime y song.duration en formato MM:SS (minutos : segundos)
+// convertir song.currentTime y song.duration en formato MM:SS (minutos : segundos)
 function formatTime(seconds) {
   let min = Math.floor(seconds / 60);
   let sec = Math.floor(seconds - min * 60);
